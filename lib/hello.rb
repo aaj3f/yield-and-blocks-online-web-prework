229ee542d54c4f819d7b8fc67872af80b0fde9e8
Andrew Johnson <ajohnson.uva@gmail.com>
2018-10-17 16:42:44 +0000

    automatically backed up by learn

--- lib/hello.rb
+++ lib/hello.rb
@@ -1,12 +1,14 @@
 def hello_t(names)
-  i = 0
+  if names.length > 0
+    i = 0
 
-  while i < names.length
-    yield(names[i])
-    i += 1
-  end
+    while i < names.length
+      yield(names[i])
+      i += 1
+    end
+  else
+    puts "Hey! No block was given!\n"
   names
 end
 
 # call your method here!
-
